155600e2de070fad9c83bcd29292ed42feabacd5
brianboy82 <brian.g.8642@gmail.com>
2020-10-28 13:22:29 -0400

    Starter for website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+// import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/core/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBarStyle: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  },
+}));
+
+// const About = (props) => {
+//   return <div> All there is to know about me </div>;
+// };
+// const Welcome = (props) => {
+//   return <div>Welcome</div>;
+// };
+
+// const Nav = (props) => {
+//   return (
+//     <nav>
+//       <ul>
+//         <li>
+//           <NavLink to="/">Home</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/about">About</NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+const App = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBarStyle}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Album Reviews
+          </Typography>
+          <Button color="inherit">Donate</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
